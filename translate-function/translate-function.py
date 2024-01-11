1d07c3ac522475ab35e3a962455fa1075ad60be5
Yoshihisa Nakai <64535376+yosnak13@users.noreply.github.com>
2024-01-11 15:38:14 +0900

    api-gatewayのgetメソッドから呼び出し (#2)

--- translate-function/translate-function.py
+++ translate-function/translate-function.py
@@ -1,11 +1,11 @@
 import json
 import boto3
 
-translate = boto3.client('translate')
+translate = boto3.client(service_name = 'translate')
 
 def lambda_handler(event, context):
 
-  input_text = "こんにちは"
+  input_text = event['queryStringParameters']['input_text']
 
   response = translate.translate_text(
     Text = input_text,
@@ -19,5 +19,7 @@ def lambda_handler(event, context):
     'statusCode': 200,
     'body': json.dumps({
       'output_text' : output_text
-    })
+    }),
+    'isBase64Encoded': False,
+    'headers': {}
   }
